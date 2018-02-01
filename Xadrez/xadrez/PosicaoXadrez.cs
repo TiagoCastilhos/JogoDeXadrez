82c014d43825e2b0cafc060d5c6124460b14d67c
Tiago Castilhos <tiagocastilhos66@gmail.com>
2018-02-01 21:36:50 -0200

    Salvar no github

--- Xadrez/xadrez/PosicaoXadrez.cs
+++ Xadrez/xadrez/PosicaoXadrez.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xadrez.Tabuleiro;
 namespace Xadrez.xadrez {
     class PosicaoXadrez {
+
+        public char coluna;
+        public int linha;
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
     }
 }
