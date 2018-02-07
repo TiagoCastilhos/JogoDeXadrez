16b0c478eedea4c65b31df377594b97d2560b391
Tiago Castilhos <tiagocastilhos66@gmail.com>
2018-02-07 18:23:41 -0200

    roques implementados

--- Xadrez/xadrez/Rei.cs
+++ Xadrez/xadrez/Rei.cs
@@ -14,6 +14,12 @@ namespace Xadrez.xadrez {
             this.partida = partida;
         }
 
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = Tabuleiro.peca(pos);
+            return p != null && p is Torre && p.Cor == Cor && p.qteMovimentos == 0;
+        }
+
         public override string ToString()
         {
             return "R";
@@ -77,6 +83,40 @@ namespace Xadrez.xadrez {
             if (Tabuleiro.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            // ROQUE PEQUENO
+            pos.definirValores(Posicao.linha - 1, Posicao.coluna);
+            if(qteMovimentos==0 && !partida.xeque)
+            {
+                Posicao posT1 = new Posicao(Posicao.linha, Posicao.coluna + 3);
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(Posicao.linha, Posicao.coluna + 1);
+                    Posicao p2 = new Posicao(Posicao.linha, Posicao.coluna + 2);
+                    if (Tabuleiro.peca(p1) == null && Tabuleiro.peca(p2) == null)
+                    {
+                        mat[Posicao.linha, Posicao.coluna + 2] = true;
+                    }
+                }
+            }
+
+            // ROQUE Grande
+            pos.definirValores(Posicao.linha - 2, Posicao.coluna);
+            if (qteMovimentos == 0 && !partida.xeque)
+            {
+                Posicao posT2 = new Posicao(Posicao.linha, Posicao.coluna - 4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(Posicao.linha, Posicao.coluna - 1);
+                    Posicao p2 = new Posicao(Posicao.linha, Posicao.coluna - 2);
+                    Posicao p3 = new Posicao(Posicao.linha, Posicao.coluna - 3);
+                    if (Tabuleiro.peca(p1) == null && Tabuleiro.peca(p2) == null && Tabuleiro.peca(p3) == null)
+                    {
+                        mat[Posicao.linha, Posicao.coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
